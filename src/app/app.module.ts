aa22dd2d269a1ccccd1e0256957ea5e81e13f0f3
pablogp8 <pablo.ys@hotmail.com>
2019-03-21 13:36:25 -0600

    agregarStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { EstadosComponent } from './estados/estados.component';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule MatInputModule} from '@angular/material';
 import { OrdenpagoComponent } from './ordenpago/ordenpago.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { OrdenpagoComponent } from './ordenpago/ordenpago.component';
     BrowserModule,
 	BrowserAnimationsModule,
 	MatButtonModule, 
-	MatCheckboxModule
+	MatCheckboxModule,
+	FormsModule,MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
